f796462fd81edbf52613edec0ca1f808acfa8ecf
Latoya Rhoden <rhoden_latoya@yahoo.com>
2020-02-14 01:23:53 -0500

    edited contact form

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,11 @@ from flask_mail import Mail
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'enter some random passphrase'
+app.config['SECRET_KEY'] = 'vgy67gg@ffRRt'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '465' # (or try 2525)
-app.config['MAIL_USERNAME'] = 'enter your mailtrap smtp
-username'
-app.config['MAIL_PASSWORD'] = 'enter your mailtrap smtp
-password'
+app.config['MAIL_USERNAME'] = '17868b07c5e498'
+app.config['MAIL_PASSWORD'] = 'd46fbde47eb103'
 
 mail = Mail(app)
 from app import views
