19c56fdfc9258f5766242c0f16d9d087ade63367
Latoya Rhoden <rhoden_latoya@yahoo.com>
2020-02-16 14:11:19 -0500

    edited contact form

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from flask_mail import Mail
-from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vgy67gg@ffRRt'
+app.config['SECRET_KEY'] = 'b7b82271107c20ca69b758bd3af6cc1a'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '465' # (or try 2525)
 app.config['MAIL_USERNAME'] = ''
 app.config['MAIL_PASSWORD'] = ''
 
-#csrf.init_app(app)
-csrf = CSRFProtect(app)
 
 mail = Mail(app)
 from app import views
-csrf = CSRFProtect(app)
